21596baf38d8311e0dcf2be721d8ceec569961bd
rjaycode <24-41849@g.batstate-u.edu.ph>
2025-11-17 16:54:28 +0800

    Create Taking inputs.java

---
+++ Taking inputs.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        Person person1 = new Person(name, age);
+
+        person1.displayInfo();
+
+        scanner.close();
+    }
+}
